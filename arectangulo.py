3a630f6d758458a8ef82880e6994ab172d239475
domingoramses1 <domingoramses1@yahoo.com.mx>
2023-01-18 10:25:14 -0600

    version1

---
+++ arectangulo.py
@@ -0,0 +1,7 @@
+print("Hola buenos dias")
+print("Meta programa que calcule el área rectangulo") 
+l1=int(input("Dame el lado 1"))
+l2=int(input("Dame el lado 2"))
+arectangulo= l1*l2
+print("El area del rectangulo es",arectangulo)
+
